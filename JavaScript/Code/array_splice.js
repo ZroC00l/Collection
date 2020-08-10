d6dfe889daf1487bcdfc96aac099cc5a89455f7e
ZroC00l <giftgabanakgosi@gmail.com>
2020-08-10 13:51:02 +0200

    update

--- JavaScript/Code/array_splice.js
+++ JavaScript/Code/array_splice.js
@@ -1,11 +1,17 @@
 /*This method takes in 3 parameters first two are arrays and the third is the index at
 which to splice the array, the method will copy the contents of array 1 into array 2 based on
-the third argument*/
+the third argument, array2 should still have its contents in tact after splicing */
 
 function splice_array(array1,array2,index){
 
-       array2.splice(index,0,...array1);
-       return array2;
+       let tempArray=array2.slice();/*copies the entire contents of array2 into a temporary array,
+                                     this is so the contents of array2 stay the same after splicing*/
+       for(let i=0;i<array1.length;i++){
+             tempArray.splice(index,0,array1[i]);
+             index++;
+       }
+
+       return tempArray;
 }
 
 
