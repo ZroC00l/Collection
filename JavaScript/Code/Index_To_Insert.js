93935304faebd62ff67d5a143d43137939f548d6
ZroC00l <giftgabanakgosi@gmail.com>
2020-11-05 18:04:59 +0200

    get index to insert method

--- JavaScript/Code/Index_To_Insert.js
+++ JavaScript/Code/Index_To_Insert.js
@@ -1,10 +1,14 @@
-function indexToIns(arr){
+function indexToIns(arr,num){
 	arr2=[...arr];
 	arr2.sort(function(a,b){return a-b;}); 
     arr=[];
     arr=[...arr2];
 
-    return arr;
+    for (var i=0;i<arr.length;i++){
+    	if (arr[i] >= num) return i;
+    }
+    
+    return arr.length;
 }
 
-console.log(indexToIns([40,50,60,1,41,19,20,5,0,39,49]));
+console.log(indexToIns([40,50,60,1,41,19,20,5,0,39,49],10));
