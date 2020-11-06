4d3ec7b41730ca174f512445e9a5aae5e372efdb
ZroC00l <giftgabanakgosi@gmail.com>
2020-11-06 08:49:29 +0200

    accessing object properties inside method using this notation

--- JavaScript/Code/OOP/Creating_An_Object.js
+++ JavaScript/Code/OOP/Creating_An_Object.js
@@ -2,9 +2,16 @@
 let dog={
 	name: "Max",
 	age: 3,
+	gender:"male",
 	numLegs:4,
 	sayAge:function(){
-	    return "My dog "+ dog.name+ " is "+ dog.age+" years old";
+	    return "My dog "+ dog.name+ " is "+ dog.age+" years old.";
+	},
+	/*This method is uses the 'this' notation to access the object's property inside the
+	 method sayGender, this example is different from the sayAge method notation
+	 */
+	sayGender: function(){
+	    return this.name +" is a "+this.gender+ " dog.";
 	}
 };
 
@@ -15,3 +22,11 @@ console.log("It has "+dog.numLegs+" legs");
 
 //Using a method/function to access an object's properties
 console.log(dog.sayAge());
+
+//Using 'this' pointer to access an object's property inside a method
+/* using the this notation prevents us from having to change all instances of
+the dot notation in case the object's name changes, for instance if the object
+dog was to be changed to cat, then we need only change the name of the object, the
+methods using with that object that call the object's parameters using this wont need to
+be changed unless the method directly uses the dot notation as dog.name instead of this.name*/
+console.log(dog.sayGender());
