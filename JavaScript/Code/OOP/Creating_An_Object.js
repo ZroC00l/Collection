528d3a75e0e15c130669b60a43626ecdf2cf261a
ZroC00l <giftgabanakgosi@gmail.com>
2020-11-06 08:22:45 +0200

    creating a method in an object

--- JavaScript/Code/OOP/Creating_An_Object.js
+++ JavaScript/Code/OOP/Creating_An_Object.js
@@ -1,5 +1,17 @@
 //This is an object call dog which has two value pairs, name and number of legs
 let dog={
 	name: "Max",
-	numLegs:4
-}
+	age: 3,
+	numLegs:4,
+	sayAge:function(){
+	    return "My dog "+ dog.name+ " is "+ dog.age+" years old";
+	}
+};
+
+//Accessing object's properties using dot notation
+
+console.log("My dog's name is:"+ dog.name);
+console.log("It has "+dog.numLegs+" legs");
+
+//Using a method/function to access an object's properties
+console.log(dog.sayAge());
