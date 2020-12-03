cead723c5e32a5eb7c5c92f53cdbeb82cf4f2b55
Thato Gift Mothusi <36993420+ZroC00l@users.noreply.github.com>
2020-12-03 07:09:59 +0200

    Add files via upload

---
+++ PostgreSQL/Assignment 1/Building.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Building {
+
+    private String Name;
+
+
+    public Building(String Name)
+    {
+        this.Name=Name;
+    }
+
+    public void setName(String Name)
+    {
+        this.Name= Name;
+    }
+
+    public String getName()
+    {
+        return Name;
+
+    }
+
+    public String toString()
+    {
+        return Name;
+    }
+
+}
